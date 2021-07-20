62e0c3d8b4b2fce82eb55530065f5a4599a0b03d
nate <natevoci@users.noreply.github.com>
2021-07-20 10:35:26 +1000

    Adding service worker

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@ import ReactDOM from "react-dom";
 
 import { App } from "~/src/components/App";
 
+if ('serviceWorker' in navigator) {
+	try {
+		navigator.serviceWorker.register(new URL('service-worker.js', import.meta.url))
+		.then((registration) => {
+			console.log(`Registration successful, scope is: ${registration.scope}`);
+		})
+		.catch((error) => {
+			console.log(`Service worker registration failed, error: ${error}`);
+		});
+	}
+	catch (error) {
+		console.log(`Service worker registration exception, error: ${error}`);
+	}
+}
+
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App />, mountNode);
---
+++ src/service-worker.js
@@ -0,0 +1,34 @@
+self.addEventListener('install', event => {
+	console.log('server-worker install');
+});
+
+self.addEventListener('activate', function(event) {
+	console.log('server-worker activate');
+	event.waitUntil(
+		// Clear cache when a new version is available
+		caches.keys().then(cacheNames => Promise.all(
+			cacheNames.map(cacheName => {
+				return caches.delete(cacheName);
+			})
+		)),
+	);
+});
+
+self.addEventListener('fetch', event => {
+	// console.log(`server-worker fetch: ${event.request.url}`);
+
+	// Just cache http and https request. (ie. don't cache chrome-extension requests)
+	if (/^https?:\/\//.exec(event.request.url)) {
+		event.respondWith(
+			caches.open('obs-tiles-static').then(cache => {
+				const r = cache.match(event.request).then(response => {
+					return response || fetch(event.request).then(response => {
+						cache.put(event.request, response.clone());
+						return response;
+					});
+				});
+				return r;
+			})
+		);
+	}
+});
