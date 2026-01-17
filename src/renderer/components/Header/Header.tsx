7b0d0a4992e1b5851cb8ae15c4d326ff178935d7
Nate <natevoci@users.noreply.github.com>
2026-01-17 17:37:17 +1100

    Added version to UI in bottom bar

--- src/renderer/components/Header/Header.tsx
+++ src/renderer/components/Header/Header.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 import { SettingsButton } from '../Settings/SettingsButton'
-import OBSLogo from '~/assets/obslogo.png?url';
+import OBSLogo from '~/assets/obslogo.png?url'
+import { APP_VERSION } from '../../version'
 
 const Wrapper = styled.div`
 	height: ${p => p.theme.grid(8)};
@@ -12,13 +13,28 @@ const Wrapper = styled.div`
 	background-color: ${p => p.theme.navBackground};
 `
 
+const LogoContainer = styled.div`
+	display: flex;
+	align-items: center;
+	gap: ${p => p.theme.grid(2)};
+`
+
 const Logo = styled.img`
     max-height: ${p => p.theme.grid(6.75)};
 `;
 
+const Version = styled.span`
+	font-size: ${p => p.theme.grid(1.5)};
+	color: ${p => p.theme.text};
+	opacity: 0.7;
+`
+
 export const Header = () => (
 	<Wrapper>
-		<Logo src={OBSLogo} alt="OBS logo" />
+		<LogoContainer>
+			<Logo src={OBSLogo} alt="OBS logo" />
+			<Version>v{APP_VERSION}</Version>
+		</LogoContainer>
 		<SettingsButton />
 	</Wrapper>
 )
